75b03196472f6e7791cdb114865e88ff12ef0135
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-10-01 19:36:50 +0200

    Update ex_8.js

--- day_4/ex_8.js
+++ day_4/ex_8.js
@@ -4,12 +4,13 @@ Write a function "upperFirst" to capitalize the first letter of each word of a g
 
 const upperFirst = (str) => {
     // array of strings
-    let arrStr = str.split('')
-    console.log(arrStr); //check
-
-
+    let arrStr = str.split(' ');
+    for (let i = 0; i < arrStr.length; i++) {
+        // getting the first char of the word and capitalizing it
+        let firstChar = arrStr[i].charAt(0).toUpperCase();
+        arrStr[i] = firstChar + arrStr[i].slice(1);
+    }
+    return arrStr.join(' ')
 }
 
-// couldnt finish
-
-console.log(upperFirst('ciao scemo, come stai?'));
+console.log(upperFirst('Morty, is the cutest sphynx cat in my house'));
